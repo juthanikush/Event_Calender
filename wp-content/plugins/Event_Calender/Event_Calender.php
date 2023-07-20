d20ce0b0998e77a11c5dd62860e9e66935a62053
KUSH  JUTHNAI <81456337+juthanikush@users.noreply.github.com>
2023-07-20 22:24:54 +0530

    remove unwanted thinks

--- wp-content/plugins/Event_Calender/Event_Calender.php
+++ wp-content/plugins/Event_Calender/Event_Calender.php
@@ -47,26 +47,14 @@ function Save_Data(){
                 if (move_uploaded_file($image['tmp_name'], $image_path)) {
                     // Image uploaded successfully, you can now save the image path or perform further actions
         
-                    // Save the image path to the database or use it as needed
-                    //$image_url = $upload_dir['url'] . '/' . $image_name;
-                    // Example: save_image_path_to_database($image_url);
-        
-                    // Display a success message
+                   
                   
                 } else {
-                    // Failed to upload the image
+                    
                     echo 'Failed to upload the image.';
                 }
             }
-            // $title=sanitize_text_field($_POST['title']);
-            // $description=sanitize_text_field($_POST['description']);
-            // $date=sanitize_text_field($_POST['date']);
-            // $time=sanitize_text_field($_POST['time']);
-            // $location=sanitize_text_field($_POST['location']);
-            // $organizer=sanitize_text_field($_POST['organizer']);
-            // $img=sanitize_text_field($_POST);
-            // $notification=sanitize_text_field($_POST['notification']);
-            //echo'<pre>';print_r($_POST);die();
+           
             $post_id = wp_insert_post(array(
                 'post_title' => $_POST['title'], // Set the post title
                 'post_type' => 'event', // Set the post type (e.g., 'post', 'page', or a custom post type)
@@ -82,12 +70,7 @@ function Save_Data(){
                 update_post_meta($post_id,  'img', $image_name);
             }
            
-            //$menu_page_url = menu_page_url('my-admin-component', true); // Replace 'menu-slug' with the slug of your menu page
-
-            // if ($menu_page_url) {
-            //     wp_redirect($menu_page_url);
-            //     exit();
-            // }
+            
             
             
         }
@@ -122,61 +105,6 @@ add_filter('template_include', 'load_custom_form_page_template');
 
 
 
-// function Save_Data_Meta_Data(){
-//     // Retrieve the form data
-//     $form_data = $_POST['form_data'];
-//     parse_str($form_data, $form_array);
-    
-//     echo '<pre>';echo($form_array);die();
-//     $image = $_FILES['image'];
-//     if ($image['name']) {
-//         // Set the upload directory and file name
-//         $upload_dir = wp_upload_dir();
-//         $image_name = $image['name'];
-//         $image_path = $upload_dir['path'] . '/' . $image_name;
-
-//         // Move the uploaded image to the destination directory
-//         if (move_uploaded_file($image['tmp_name'], $image_path)) {
-//             // Image uploaded successfully, you can now save the image path or perform further actions
-
-//             // Save the image path to the database or use it as needed
-//             //$image_url = $upload_dir['url'] . '/' . $image_name;
-//             // Example: save_image_path_to_database($image_url);
-
-//             // Display a success message
-//             echo 'Image uploaded successfully!';
-//         } else {
-//             // Failed to upload the image
-//             echo 'Failed to upload the image.';
-//         }
-//     }
-//     $title=sanitize_text_field($form_array['title']);
-//     $description=sanitize_text_field($form_array['description']);
-//     $date=sanitize_text_field($form_array['date']);
-//     $time=sanitize_text_field($form_array['time']);
-//     $location=sanitize_text_field($form_array['location']);
-//     $organizer=sanitize_text_field($form_array['organizer']);
-//     $img=sanitize_text_field($image_name);
-//     $notification=sanitize_text_field($form_array['notification']);
-    
-//     $post_id = wp_insert_post(array(
-//         'post_title' => $title, // Set the post title
-//         'post_type' => 'event', // Set the post type (e.g., 'post', 'page', or a custom post type)
-//         'post_status' => 'publish' // Set the post status (e.g., 'publish', 'draft', etc.)
-//     ));
-//     update_post_meta($post_id, 'title', $title);
-//     update_post_meta($post_id, 'description', $description);
-//     update_post_meta($post_id, 'date', $date);
-//     update_post_meta($post_id, 'time', $time);
-//     update_post_meta($post_id, 'location', $location);
-//     update_post_meta($post_id, 'organizer', $organizer);
-//     update_post_meta($post_id, 'img', $img);
-//     update_post_meta($post_id, 'notification', $notification);
-    
-//     echo "Success";
-//     wp_die();
-// }
-//add_action('wp_ajax_Save_Data_Meta_Data', 'Save_Data_Meta_Data');
 
 function add_Event_Calender(){
     wp_enqueue_style('admin-style',plugins_url('include/css/style.css',__FILE__));
